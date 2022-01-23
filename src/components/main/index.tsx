625aa0c427ca7076c05b3642828c9af852776177
Juan Cruz Medina <juancruzfaq@gmail.com>
2022-01-23 18:41:21 -0300

    Se arregla la foto

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,6 +1,5 @@
 import { PageTitle } from "@components/page-titles";
 import React from "react";
-import logo from "logo.png";
 
 import { Wrapper, Container } from "./styled";
 
@@ -9,7 +8,7 @@ export const Main: React.FC = () => {
     <Wrapper>
       <Container>
         <h1>Juan Cruz Medina</h1>
-        <img src={logo} alt="Logo" />
+        <img src="./logo.png" />
         <p>Back End Developer &#38; Engineering Student</p>
       </Container>
     </Wrapper>
